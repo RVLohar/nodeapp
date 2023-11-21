0f727bfaac7fc5c5dddf07b3769fd1e5bf0f1346
rohanlohar <rohanlohar3107@gmail.com>
2023-11-21 15:23:41 +0530

    navin commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express= require('express');
 var app= express ();
 app.get ('/', function (req, res) {
-res.send ('helllo world');
+res.send ('hello joker');
 });
 app.listen(process.env.PORT || 80);
 midule.exports= app;
---
+++ test.js
@@ -0,0 +1,5 @@
+var resquest= require('supertset')
+var app= require('./index.js); describe('GET/',function(){
+request(app).get('/'.expect('hello jocker',done);
+});
+});
